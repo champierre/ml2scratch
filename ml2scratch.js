0468a6bc1674951ba7d0f975b1aff80ac983c2e5
Junya Ishihara <junya@champierre.com>
2018-04-15 17:10:58 +0900

    Add when_received_0,1,2 blocks.

--- ml2scratch.js
+++ ml2scratch.js
@@ -1,6 +1,7 @@
 (function(ext) {
     var ws;
     var when_received = false;
+    var when_received_arr = Array(3).fill(false);
     var class_index;
     var conn_id;
 
@@ -11,13 +12,18 @@
     };
 
     ext.connect = function(_conn_id) {
+      if (_conn_id.length == 0) {
+        alert('Blank ID is invalid.');
+        return;
+      }
       conn_id = _conn_id;
       ws = new WebSocket('wss://ml2scratch-helper.glitch.me/');
       ws.onmessage = function(evt) {
         data = JSON.parse(evt.data);
         if (data.action == 'predict') {
-          class_index = data.value;
+          class_index = parseInt(data.value, 10);
           when_received = true;
+          when_received_arr[class_index] = true
         }
       }
       ws.onopen = function(evt) {
@@ -33,6 +39,30 @@
       return false;
     }
 
+    ext.when_received_0 = function() {
+      if (when_received_arr[0]) {
+        when_received_arr[0] = false;
+        return true;
+      }
+      return false;
+    }
+
+    ext.when_received_1 = function() {
+      if (when_received_arr[1]) {
+        when_received_arr[1] = false;
+        return true;
+      }
+      return false;
+    }
+
+    ext.when_received_2 = function() {
+      if (when_received_arr[2]) {
+        when_received_arr[2] = false;
+        return true;
+      }
+      return false;
+    }
+
     ext.class_index = function() {
       return class_index;
     }
@@ -42,11 +72,17 @@
         ja: {
             connect: 'ID: %s で接続する',
             when_received: '分類を受け取ったとき',
+            when_received_0: '「分類0」を受け取ったとき',
+            when_received_1: '「分類1」を受け取ったとき',
+            when_received_2: '「分類2」を受け取ったとき',
             class_index: '分類'
         },
         en: {
             connect: 'Connect with ID: %s',
             when_received: 'when received',
+            when_received_0: 'when received label 0',
+            when_received_1: 'when received label 1',
+            when_received_2: 'when received label 2',
             class_index: 'class index'
         },
     }
@@ -55,6 +91,9 @@
         blocks: [
             [' ', locale[lang].connect, 'connect', ''],
             ['h', locale[lang].when_received, 'when_received'],
+            ['h', locale[lang].when_received_0, 'when_received_0'],
+            ['h', locale[lang].when_received_1, 'when_received_1'],
+            ['h', locale[lang].when_received_2, 'when_received_2'],
             ['r', locale[lang].class_index, 'class_index']
         ]
     };
