e0413f64ed199250ee2e3d546af0d0a4307b9973
Junya Ishihara <junya@champierre.com>
2018-08-12 16:33:22 +0900

    Start to apply new design.

--- app/javascripts/scripts.js
+++ app/javascripts/scripts.js
@@ -1,7 +1,53 @@
 import $ from 'jquery'
 import 'bootstrap'
 
+const IMAGE_SIZE = 270;
+let videoPlaying = false;
+
 $(function () {
+  // Setup webcam
+  let video = $('video')[0];
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (stream) {
+    video.srcObject = stream;
+    // _this.video.width = IMAGE_SIZE;
+    // _this.video.height = IMAGE_SIZE;
+
+    video.addEventListener('playing', function () {
+      videoPlaying = true;
+    });
+    video.addEventListener('paused', function () {
+      videoPlaying = false;
+    });
+  });
+
+  $('#add-card').on('click', function(e) {
+    const content =`
+    <div class="card-block">
+      <div class="dammy-photo"></div>
+
+      <div class="card-block__info">
+        <span>
+          <i class="icon-photos"></i>
+          x 5
+        </span>
+        <div class="dropdown">
+          <a href="#" class="link" data-toggle="dropdown">
+            <i class="icon-dots-white"></i>
+          </a>
+          <div class="card-dropdown-menu dropdown-menu dropdown-menu-right">
+            <a class="dropdown-item" href="#">リセット</a>
+          </div>
+        </div>
+      </div>
+
+      <a href="#" class="button is-w100 is-mini">
+        <i class="icon-camera is-mini"></i>
+      </a>
+    </div>
+    `;
+
+    return false;
+  });
 
   // fileを選択したら名前を表示させる
   $('[data-file]').each(function(index, el) {
