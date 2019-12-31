0460eae6dd9fa0dbcbf6c0caeba02341ba9ca1b5
Junya Ishihara <junya@champierre.com>
2019-12-31 22:48:27 +0900

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -13,4 +13,24 @@ mkdir -p src/lib/libraries/extensions/ml2scratch
 wget -P src/lib/libraries/extensions/ml2scratch https://raw.githubusercontent.com/champierre/ml2scratch/master/scratch-gui/src/lib/libraries/extensions/ml2scratch/ml2scratch.png
 wget -P src/lib/libraries/extensions/ml2scratch https://raw.githubusercontent.com/champierre/ml2scratch/master/scratch-gui/src/lib/libraries/extensions/ml2scratch/ml2scratch-small.png
 mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
-sed -e "s|^export default \[$|import ml2scratchIconURL from './ml2scratch/ml2scratch.png';${LF}import ml2scratchInsetIconURL from './ml2scratch/ml2scratch-small.png';${LF}${LF}export default [|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
+ML2SCRATCH="\
+    {${LF}\
+        name: 'ML2Scratch',${LF}\
+        extensionId: 'ml2scratch',${LF}\
+        collaborator: 'champierre',${LF}\
+        iconURL: ml2scratchIconURL,${LF}\
+        insetIconURL: ml2scratchInsetIconURL,${LF}\
+        description: (${LF}\
+            <FormattedMessage${LF}\
+                defaultMessage='ML2Scratch Blocks.'${LF}\
+                description='ML2Scratch Blocks'${LF}\
+                id='gui.extension.ml2scratchblocks.description'${LF}\
+            />${LF}\
+        ),${LF}\
+        featured: true,${LF}\
+        disabled: false,${LF}\
+        internetConnectionRequired: true,${LF}\
+        bluetoothRequired: false,${LF}\
+        helpLink: 'https://champierre.github.io/ml2scratch/'${LF}\
+    },"
+sed -e "s|^export default \[$|import ml2scratchIconURL from './ml2scratch/ml2scratch.png';${LF}import ml2scratchInsetIconURL from './ml2scratch/ml2scratch-small.png';${LF}${LF}export default [${LF}${ML2SCRATCH}|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
